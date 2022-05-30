738315647155d52313bc4c7c52a325650e24f347
Sandra Charles <105268588+sandracharles007@users.noreply.github.com>
2022-05-30 17:14:36 +0530

    Add files via upload

---
+++ Area of the circle.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 21:34:10 2022
+
+@author: Sandra
+"""
+
+r = float(input("Input the radius of the circle : "))
+print("The area of the circle with radius",r,"is :",3.142857*r*r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Extension of file.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 17:35:51 2022
+
+@author: Sandra
+"""
+
+file = str( input("Input the file name :"))
+a = file.split('.') 
+print("The extension of the file is :",a[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
