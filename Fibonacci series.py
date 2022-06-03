744522e39ba275b6e120d0a53f6b2e512c48b638
Sandra Charles <105268588+sandracharles007@users.noreply.github.com>
2022-06-03 11:38:10 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 16:19:13 2022
+
+@author: Sandra
+"""
+n = int(input("NUMBER OF TERMS REQUIRED: "))
+print("FIBONACCI SERIES: ")
+a = 0
+b = 1
+for i in range(n):
+    print(a)
+    z = a+b
+    b = z
+    a = b
+    
+    
+   
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
