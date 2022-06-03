0cf4a33006cd7274ea167614ff5a9d2b30d4ae3e
Sandra Charles <105268588+sandracharles007@users.noreply.github.com>
2022-06-03 11:50:39 +0530

    Add files via upload

---
+++ Positive integers.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 15:30:50 2022
+
+@author: Sandra
+"""
+list=[]
+n = int(input("NO OF ELEMENTS IN THE LIST: "))
+for i in range(n):
+    e = int(input("ELEMENTS OF THE LIST: "))
+    list.append(e)
+    
+    print(list)
+
+print("POSITIVE NUMBERS IN",list,"ARE: ")  
+for x in list:
+    if x>=0:
+        print(x)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
